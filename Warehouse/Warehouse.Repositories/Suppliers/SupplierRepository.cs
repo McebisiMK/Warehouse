17e49ac045ebeb367671a893596dccdf6da9223d
Tusani Gwala <TusaniG@SAHOMELOANS.COM>
2019-06-19 12:38:11 +0200

    small error fix

--- Warehouse/Warehouse.Repositories/Suppliers/SupplierRepository.cs
+++ Warehouse/Warehouse.Repositories/Suppliers/SupplierRepository.cs
@@ -1,15 +1,14 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using Warehouse.Entities;
+﻿using Warehouse.Entities;
+using Warehouse.GenericRepository;
 using Warehouse.IRepositories.Suppliers;
 
 namespace Warehouse.Repositories.Suppliers
 {
-    public class SupplierRepository : ISupplierRepository
+    public class SupplierRepository : GenericRepository<Supplier>, ISupplierRepository
     {
         private WarehouseEntities context;
 
-        public SupplierRepository(WarehouseEntities context)
+        public SupplierRepository(WarehouseEntities context) : base(context)
         {
             this.context = context;
         }
